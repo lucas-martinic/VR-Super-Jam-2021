ba637a4a11858e8e7846492f9b091dad7bfb1b2f
lucas martinic <lucas.martinic@gmail.com>
2021-10-22 17:46:35 +0200

    Basic VR setup

---
+++ VRSuperJam/Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandAnimator : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public ActionBasedController controller = null;
+    [SerializeField] private bool leftHand;
+    private Animator animator = null;
+
+    private readonly List<Finger> gripfingers = new List<Finger>()
+    {
+        new Finger(FingerType.Middle),
+        new Finger(FingerType.Ring),
+        new Finger(FingerType.Pinky)
+    };
+
+    private readonly List<Finger> pointFingers = new List<Finger>
+    {
+        new Finger(FingerType.Index),
+        new Finger(FingerType.Thumb)
+    };
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponentInParent<ActionBasedController>();
+    }
+
+    private void Update()
+    {
+        if (controller == null) return;
+        // Store input
+        CheckGrip();
+        CheckPointer();
+
+        // Smooth input values
+        SmoothFinger(pointFingers);
+        SmoothFinger(gripfingers);
+
+        // Apply smoothed values
+        AnimateFinger(pointFingers);
+        AnimateFinger(gripfingers);
+    }
+
+    private void CheckGrip()
+    {
+        float gripValue = controller.selectAction.action.ReadValue<float>();
+        SetFingerTargets(gripfingers, gripValue);
+    }
+
+    private void CheckPointer()
+    {
+        float gripValue = controller.activateAction.action.ReadValue<float>();
+        SetFingerTargets(pointFingers, gripValue);
+    }
+
+    private void SetFingerTargets(List<Finger> fingers, float value)
+    {
+        foreach (Finger finger in fingers)
+            finger.target = value;
+    }
+
+    private void SmoothFinger(List<Finger> fingers)
+    {
+        foreach(Finger finger in fingers)
+        {
+            float time = speed * Time.unscaledDeltaTime;
+            finger.current = Mathf.MoveTowards(finger.current, finger.target, time);
+        }
+    }
+
+    private void AnimateFinger(List<Finger> fingers)
+    {
+        foreach (Finger finger in fingers)
+            AnimateFinger(finger.type.ToString(), finger.current);
+    }
+
+    private void AnimateFinger(string finger, float blend)
+    {
+        animator.SetFloat(finger, blend);
+    }
+}
