0918dfde2de230fb932226a257ef882f554b9e32
lucas martinic <lucas.martinic@gmail.com>
2021-10-23 17:34:34 +0200

    Invisible objects only collide when visible

--- VRSuperJam/Assets/Scripts/InvisibleObject.cs
+++ VRSuperJam/Assets/Scripts/InvisibleObject.cs
@@ -1,15 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InvisibleObject : MonoBehaviour
 {
-    private Renderer renderer;
+    private Renderer _renderer;
+    private Collider _collider;
+
     void Start()
     {
-        renderer = GetComponent<Renderer>();
-        Color _color = renderer.material.color;
-        renderer.material = new Material(GameManager.Instance.invisibleMaterial);
-        renderer.material.color = _color;
+        _renderer = GetComponent<Renderer>();
+        _collider = GetComponent<Collider>();
+        Color _color = _renderer.material.color;
+        _renderer.material = new Material(GameManager.Instance.invisibleMaterial);
+        _renderer.material.color = _color;
+        _collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Gravity")) {
+            if(_collider != null) {
+                _collider.isTrigger = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Gravity")) {
+            if(_collider != null) {
+                 _collider.isTrigger = true;
+            }
+        }
     }
 }
