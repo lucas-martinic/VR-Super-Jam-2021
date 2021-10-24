21f95ab9b9cfdd3250baaabfeba225ccd0fcef07
lucas martinic <lucas.martinic@gmail.com>
2021-10-24 13:19:41 +0200

    DecoloredRenderer can have texture

--- VRSuperJam/Assets/Scripts/DecoloredRenderer.cs
+++ VRSuperJam/Assets/Scripts/DecoloredRenderer.cs
@@ -6,7 +6,11 @@ public class DecoloredRenderer : MonoBehaviour {
     void Start() {
         _renderer = GetComponent<Renderer>();
         Color _color = _renderer.material.color;
+        Texture texture = _renderer.material.mainTexture;
         _renderer.material = new Material(GameManager.Instance.decoloredMaterial);
         _renderer.material.color = _color;
+        if(texture != null) {
+            _renderer.material.mainTexture = texture;
+        }
     }
 }
