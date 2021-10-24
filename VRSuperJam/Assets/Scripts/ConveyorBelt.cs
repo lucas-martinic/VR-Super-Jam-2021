513a767ed268629b8414e192305dcf4abef2913c
lucas martinic <lucas.martinic@gmail.com>
2021-10-24 22:04:27 +0200

    Conveyor belt texture

--- VRSuperJam/Assets/Scripts/ConveyorBelt.cs
+++ VRSuperJam/Assets/Scripts/ConveyorBelt.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class ConveyorBelt : MonoBehaviour
 {
     private Rigidbody body = null;
+    private Renderer _renderer;
+
+    private void Start() {
+        _renderer = GetComponent<Renderer>();
+    }
 
     private void Update() {
         if(body != null) {
-            body.transform.position += -transform.right / 2 * Time.deltaTime;
+            body.transform.position += -transform.right * Time.deltaTime / 3;
         }
+        _renderer.material.mainTextureOffset += new Vector2(1f, 0) * Time.deltaTime / 3; ;
     }
 
     private void OnTriggerEnter(Collider other) {
